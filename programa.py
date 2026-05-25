3a1e42fb5cb7efa87a24fac53ee2f97ba7cee959
JPpittigotti <jppittigotti@gmail.com>
2026-05-25 10:59:50 -0500

    Agrego aporte personal al programa (JPPitti)

--- programa.py
+++ programa.py
@@ -1,7 +1,14 @@
 # programa.py
 # Programa base para el taller colaborativo
 
-def saludar():
-    print("Hola, este es el programa base del taller")
+## Funcion de saludar
+def saludar(): 
+    print("Hola, este es el programa base del taller") 
+    print("Aporte de Jean Paul Pitti") 
+
+## Funcion de despedida
+def despedir(): 
+    print("Hasta luego, aporte de Pitti") 
 
 saludar()
+despedir()
