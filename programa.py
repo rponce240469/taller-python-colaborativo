a6c135b9af7f3b11fb279ae0035187cfb06110ff
Batista <moraleshansel985@gmail.com>
2026-05-25 11:04:26 -0500

    Agrega aporte de Hansel

--- programa.py
+++ programa.py
@@ -1,7 +1,4 @@
-# programa.py
-# Programa base para el taller colaborativo
-
 def saludar():
     print("Hola, este es el programa base del taller")
-
+    print("Aporte de Hansel Batista")
 saludar()
